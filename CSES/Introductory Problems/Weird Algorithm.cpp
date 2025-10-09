91f81aa1528e426cb436c80e391e8efff0bba97a
MohamedAnasBenMim <medanasbenmim888@gmail.com>
2025-10-09 22:57:57 +0100

    competetive programming

---
+++ CSES/Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) {
+        long long n;
+        cin>>n;
+        cout<<n<<" ";
+        while(n!=1) {
+            if(n%2==0) {
+                n=n/2;
+                cout<<n<<" ";
+            }
+            else {
+                n=n*3;
+                n++;
+                cout<<n<<" ";
+            }
+        }
+    }
+    return 0;
+}
